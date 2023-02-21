001b2898e28f17409fea92b4206c646b09c980b4
Jack Dunlavey <dunlavey2003@gmail.com>
2023-02-21 19:12:43 +0000

    Renamed 'main()' to 'river_puzzle()'. Made 'river_puzzle()' more efficient.

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
-def main():
+def river_puzzle():
     lake_positions = {'Farmer':False,'Wolf':False,'Goat':False,'Cabbage':False}
     while True:
+        print()
         display_positions(lake_positions)
         choice = input('Type what to move (or Nothing): ').lower()
+        if choice.startswith('w') and can_transport_wolf(lake_positions):
+            lake_positions = transport_object(lake_positions, 'Wolf')
 
-        if choice.startswith('w') and can_transport_wolf_successfully(
-                lake_positions):
-            lake_positions['Farmer'] = not lake_positions['Farmer']
-            lake_positions['Wolf'] = not lake_positions['Wolf']
+        elif choice.startswith('g') and can_transport_goat(lake_positions):
+            lake_positions = transport_object(lake_positions, 'Goat')
 
-        elif choice.startswith('g') and can_transport_goat_successfully(
-                lake_positions):
-            lake_positions['Farmer'] = not lake_positions['Farmer']
-            lake_positions['Goat'] = not lake_positions['Goat']
+        elif choice.startswith('c') and can_transport_cabbage(lake_positions):
+            lake_positions = transport_object(lake_positions, 'Cabbage')
 
-        elif choice.startswith('c') and can_transport_cabbage_successfully(
-                lake_positions):
-            lake_positions['Farmer'] = not lake_positions['Farmer']
-            lake_positions['Cabbage'] = not lake_positions['Cabbage']
-
-        elif choice.startswith('n') and can_transport_nothing_successfully(
-                lake_positions):
-            lake_positions['Farmer'] = not lake_positions['Farmer']
+        elif choice.startswith('n') and can_transport_nothing(lake_positions):
+            lake_positions = transport_object(lake_positions, None)
 
         else: print('Impossible Action')
 
         if win_condition(lake_positions):
             print('Winner!')
             break
-        print()
 
 
-def display_positions(lake_pos):
+def display_positions(lake_pos) -> None:
     print(f'Farmer:{left_or_right(lake_pos["Farmer"])}')
     print(f'Wolf:{left_or_right(lake_pos["Wolf"])}')
     print(f'Goat:{left_or_right(lake_pos["Goat"])}')
@@ -42,7 +34,7 @@ def left_or_right(item) -> str:
     return 'Right' if item else 'Left'
 
 
-def can_transport_wolf_successfully(lake_pos):
+def can_transport_wolf(lake_pos) -> bool:
     """
     Returns True if the farmer and the wolf are on the same side and
     if the goat is not left on the same side as the cabbage
@@ -51,14 +43,14 @@ def can_transport_wolf_successfully(lake_pos):
     lake_pos['Goat'] != lake_pos['Cabbage']
 
 
-def can_transport_goat_successfully(lake_pos):
+def can_transport_goat(lake_pos) -> bool:
     """
     Returns True if the farmer and the goat are on the same side
     """
-    return lake_pos['Farmer'] is lake_pos['Goat'] # True if on the same side
+    return lake_pos['Farmer'] is lake_pos['Goat']
 
 
-def can_transport_cabbage_successfully(lake_pos):
+def can_transport_cabbage(lake_pos) -> bool:
     """
     Returns True if the farmer and the cabbage are on the same side and
     if the wolf is not left on the same side as the goat
@@ -67,7 +59,7 @@ def can_transport_cabbage_successfully(lake_pos):
     lake_pos['Wolf'] != lake_pos['Goat']
 
 
-def can_transport_nothing_successfully(lake_pos):
+def can_transport_nothing(lake_pos) -> bool:
     """
     Returns True if the wolf is not left on the same side as the goat and
     if the goat is not left on the same side as the cabbage
@@ -76,9 +68,23 @@ def can_transport_nothing_successfully(lake_pos):
     lake_pos['Goat'] != lake_pos['Cabbage']
 
 
-def win_condition(lake_pos):
+def transport_object(lake_pos, obj_to_trans) -> dict:
+    """
+    The Farmer always changes sides when this function runs.
+    A second object also changes side if obj_to_trans doesn't contain None.
+    """
+    lake_pos['Farmer'] = not lake_pos['Farmer']
+    if obj_to_trans:
+        lake_pos[obj_to_trans] = not lake_pos[obj_to_trans]
+    return lake_pos
+
+
+def win_condition(lake_pos) -> bool:
+    """
+    Returns True if the Wolf, Goat, and Cabbage are all True
+    """
     return lake_pos['Wolf'] == lake_pos['Goat'] == lake_pos['Cabbage'] == True
 
 
 if __name__ == '__main__':
-    main()
+    river_puzzle()
